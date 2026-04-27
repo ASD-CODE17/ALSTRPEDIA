f05a9387e865f83443106347da698494846d7fde
ASD-CODE17 <itsdoo254@gmail.com>
2026-04-27 15:10:44 +0700

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,6 @@
+global.feenya = 500 //atur fee nya misal harga asli 1.480 + fee 200 jadi harganya 1.680
+global.apikey = "UerH6X0fp5otss_WDjAMNkQCeSY45il1" //isi apikey dari https://ciaatopup.my.id minta verifikasi h2h ke admin nya
+
+// https://www.ciaatopup.com/dashboard
+
+
